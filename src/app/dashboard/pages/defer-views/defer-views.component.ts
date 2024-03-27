ddc53698f729ee6a5ce41ea485c0eb0385cecbd3
AsusMiguel <migalvez2002@gmail.com>
2024-03-26 22:14:35 -0500

    completed section

--- src/app/dashboard/pages/defer-views/defer-views.component.ts
+++ src/app/dashboard/pages/defer-views/defer-views.component.ts
@@ -1,10 +1,13 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { HeavyLoadersSlowComponent } from '@shared/heavy-loaders/heavy-loaders-slow.component';
+import { TitleComponent } from "../../../shared/title/title.component";
 
 @Component({
-  standalone: true,
-  imports: [],
-  templateUrl: './defer-views.component.html',
-  styles: ``
+    standalone: true,
+    templateUrl: './defer-views.component.html',
+    styles: ``,
+    imports: [CommonModule, HeavyLoadersSlowComponent, TitleComponent]
 })
 export default class DeferViewsComponent {
 
--- src/app/dashboard/pages/user/user.component.ts
+++ src/app/dashboard/pages/user/user.component.ts
@@ -1,11 +1,48 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, computed, inject } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { TitleComponent } from '@shared/title/title.component';
+import { toSignal } from '@angular/core/rxjs-interop';
+import { switchMap } from 'rxjs';
+import { UsersService } from '@services/users.service';
 
 @Component({
   standalone: true,
-  imports: [],
-  templateUrl: './user.component.html',
-  styles: ``
+  imports: [CommonModule, TitleComponent],
+  template: `
+    <app-title [title]="titleLabel()"></app-title>
+
+    @if ( user() ){
+    <section>
+      <img [srcset]="user()!.avatar" alt="user()?.firstname" />
+
+      <div>
+        <h3>{{ user()!.first_name }} {{ user()!.last_name }}</h3>
+        <p>{{ user()!.email }}</p>
+      </div>
+    </section>
+    }@else {
+    <p>Cargando Información</p>
+    }
+  `,
 })
 export default class UserComponent {
+  private route = inject(ActivatedRoute);
+  private usersService = inject(UsersService);
 
+  public user = toSignal(
+    this.route.params.pipe(
+      switchMap(({ id }) => this.usersService.getUserById(id))
+    )
+  ); //toSignal toma un observable y lo vuelve señal
+
+  public titleLabel = computed(() => {
+    if (this.user()) {
+      return `Información del usuario ${this.user()?.first_name} ${
+        this.user()?.last_name
+      }`;
+    }
+
+    return 'Información del usuario';
+  });
 }
