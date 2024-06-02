47b0ab1b4849b8740bdb191854a870458611ffb7
AsusMiguel <migalvez2002@gmail.com>
2024-06-01 21:07:03 -0500

    new component for angular 18

---
+++ src/app/dashboard/pages/input-output/input-output.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-input-output',
+  standalone: true,
+  imports: [],
+  templateUrl: './input-output.component.html',
+  styleUrl: './input-output.component.css'
+})
+export class InputOutputComponent {
+
+}
